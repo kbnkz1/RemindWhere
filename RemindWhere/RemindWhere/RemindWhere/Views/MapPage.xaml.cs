1a843adfc8215c4a94cbdb4d43ee556cd62240f6
kbnkz1 <48408784+kbnkz1@users.noreply.github.com>
2019-03-25 15:33:40 -0700

    Map Page and ViewModel created.

---
+++ RemindWhere/RemindWhere/RemindWhere/Views/MapPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace RemindWhere.Views
+{
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
