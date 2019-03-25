18c126745ebf38c281fe5ae4226291216aa41450
kbnkz1 <48408784+kbnkz1@users.noreply.github.com>
2019-03-25 15:55:05 -0700

    Scaffolding.

---
+++ RemindWhere/RemindWhere.API/Services/NoteService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemindWhere.API.Services
+{
+    public class NoteService
+    {
+    }
+}
